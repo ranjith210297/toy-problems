cd57a7d288ec2b8430e77e4b8496ab5204669548
ranjith210297 <ranjeeth2197@msitprogram.net>
2020-04-15 08:42:41 +0530

    Testing the methods, errors occured. Resolving

--- LRU.py
+++ LRU.py
@@ -5,11 +5,33 @@ class LRU:
 		self.lrucache = {}
 
 
-	def get(self,item):
-		pass
+	def get(self,item,default = None):
+		if item in self.lru:
+			return self.lrucache[item]
+		else:
+			return default
 
 	def put(self,item):
-		pass
+		if(len(self.lru) < self.size):
+			if key in self.lru:
+				self.lru.remove(item)
+				self.lru.append(item)
+				self.lrucache[item] = item
+			else:
+				self.lru.append(item)
+				self.lrucache[item] = item
+			return "Successful"
+
+		else:
+			var = self.lru.pop()
+			self.lru.append(item)
+			del self.lrucache[var]
+			self.lrucache[item] = item
+			return "Successful"
 
 	def get_cache(self):
-		pass
+		ls = []
+		for item in reversed(self.lru):
+			ls.append(item+"-"+self.lrucache[item])
+		#return get-cache
+		print(ls)
